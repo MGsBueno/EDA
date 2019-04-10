ce18c0b8ca5f0f52c183934261eb549bbf834c2e
MGsBueno <maurobueno@id.uff.br>
2019-04-10 13:55:17 -0300

    add line functions

---
+++ filas-listas.h
@@ -0,0 +1,13 @@
+
+typedef struct fila {
+    TLista *inicio;
+    TLista *fim;
+} TFila;
+
+TFila* inicializaf(void);
+int fila_vazia(TFila *f);
+void insere(TFila *f, int elem);
+int retira(TFila *f);
+void liberaf(TFila *f);
+void imprime_fila(TFila *f);
+
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "pilha-lista.h"
-#include "stdio.h"
-
+#include <stdio.h>
+#include "filas-listas.h"
 /*
  *  ------ Início das funções da pilha ------  *
 */
@@ -88,7 +88,7 @@ void altera(TPilha *pilha, int novo){
  *
  ------------------------------------------------------------------------------
  *
- * * ------ Início das funções Fila ------ *
+ * * ------ Início das funções Lista ------ *
  */
 
 TLista* cria_lista(void){
@@ -160,9 +160,103 @@ void imprime(TLista *li) {
 
 }
 /*
- * ------ Fim das funchoes Fila ------ * *
+ * ------ Fim das funchoes Lista ------ * *
  */
+
+
+
+/*
+ * ------ Início das funchoes Fila ------ * *
+ */
+TFila* inicializaf(void){
+    TFila *f = (TFila *)malloc(sizeof(TFila));
+    f->inicio = NULL;
+    f->fim = NULL;
+    return f;
+}
+
+int fila_vazia(TFila *f){
+    if (f->inicio == NULL) {
+        return 1;
+    }
+    else return 0;
+}
+
+void insere(TFila *f, int elem){
+    TLista *novo = (TLista *)malloc(sizeof(TLista));
+    novo->info = elem;
+    novo->prox = NULL; //inserção no fim da fila
+    if (!fila_vazia(f)){
+        f->fim->prox = novo;
+    }
+    else{
+        f->inicio = novo;
+    }
+    f->fim = novo; //elemento inserido é o novo fim da fila
+}
+
+/*
+ * Remove elemento da fila e retorna info do elemento excluído
+ */
+int retira(TFila *f){
+    if (fila_vazia(f)){
+        exit(1);
+    }
+    int info = f->inicio->info;
+
+    TLista *aux = f->inicio;
+    f->inicio=f->inicio->prox;
+    //se elemento removido era o único da fila
+    //faz fim apontar para NULL também
+    if (f->inicio == NULL) {
+        f->fim = NULL;
+    }
+    free(aux);
+    return info;
+}
+
+void liberaf(TFila *f){
+    TLista *q, *p = f->inicio;
+    while(p != NULL){
+        q = p;
+        p = p->prox;
+        free(q);
+    }
+    free(f);
+}
+
+void imprime_fila(TFila *f) {
+    if (f->inicio!=NULL){
+        printf("%d",f->inicio->info);
+    TLista* q = f->inicio;
+        if (q->prox!=NULL) {
+            f->inicio = q->prox;
+            imprime_fila(f);
+            f->inicio = q;
+        }
+    }
+}
+
+/*
+ * ------ Fim das funchoes Lista ------ * *
+ */
+
 int main() {
+    TFila *fila = inicializaf();
+    insere(fila, 5);
+    insere(fila, 3);
+    insere(fila, 2);
+    imprime_fila(fila);
+
+    int x = retira(fila);
+    printf("\nelemento %d retirado da fila\n", x);
+    imprime_fila(fila);
+
+    liberaf(fila);
+
+
+
+    /*
     printf("exercicio de lista encadeada\n");
     TLista *lista;
     lista = cria_lista();
@@ -196,6 +290,7 @@ int main() {
     altera(lista, 11,15);
     printf("atual\n");
     imprime(lista);
+    */
 
 
     /*
